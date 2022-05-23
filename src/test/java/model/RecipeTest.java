cd928f2d1c673922ab32c8d0b378fc65a5d3de89
ynyanchoka <monaryymelda@gmail.com>
2022-05-23 23:59:18 +0300

    add getTitle and getPrepTime tests

---
+++ src/test/java/model/RecipeTest.java
@@ -0,0 +1,40 @@
+package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeTest {
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+    @Test
+    public void instantiatesRecipeObjectsCorrectly_true() throws Exception {
+        Recipe testRecipe = setUpRecipe();
+        assertEquals(true, testRecipe instanceof Recipe);
+    }
+
+    @Test
+    public void getTitleReturnsCorrectTitle() throws Exception {
+        Recipe testRecipe = setUpRecipe();
+        assertEquals("Biryani", testRecipe.getTitle());
+    }
+    @Test
+    public void getPrepTimeReturnsCorrectTime() throws Exception {
+        Recipe testRecipe = setUpRecipe();
+        assertEquals(20, testRecipe.getPrepTime());
+    }
+
+
+    //HELPER
+    public Recipe setUpRecipe (){
+        return new Recipe ("Biryani", 20, 30, 10,"Rice","Stir","Blue");
+    }
+
+}
