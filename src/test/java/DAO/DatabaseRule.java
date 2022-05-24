ef5ca557303cdf7a244ca8e8c10379bd0cfebc4f
Charawey-X <charawey@gmail.com>
2022-05-24 17:55:50 +0300

    tests passing

--- src/test/java/DAO/DatabaseRule.java
+++ src/test/java/DAO/DatabaseRule.java
@@ -1,6 +1,5 @@
 package DAO;
 
-import Database.DB;
 import org.junit.rules.ExternalResource;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -9,7 +8,6 @@ public class DatabaseRule extends ExternalResource {
 
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/recipe_app_test", "x", "230620");
     Connection connection;
-    public int id;
 
     @Override
     protected void before() {
